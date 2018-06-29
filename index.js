72524e66fd1afb133dcca8706fa8774836941ac6
Michael Ballard <ballard.michael9@gmail.com>
2018-06-29 08:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+class Driver {
+  constructor(name,joined){
+    this.name = name 
+    this.joined = joined
+  }
+  
+  
+  startDate(){
+    return new Date(this.joined).to_s
+  }
+  
+  yearsExperienceFromBeginningOf(year){
+    let endDate = new Date(year,1,1)
+    let beginningDate = this.startDate()
+ 
+    return endDate - startDate
+  }
+}
